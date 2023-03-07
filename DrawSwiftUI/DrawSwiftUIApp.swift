5983c3e8e3db97b96dfcb4608761c03053331eba
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-03-07 12:39:06 +0200

    Initial Commit

---
+++ DrawSwiftUI/DrawSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawSwiftUIApp.swift
+//  DrawSwiftUI
+//
+//  Created by Vladimir Dvornikov on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
